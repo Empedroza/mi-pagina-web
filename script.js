dcd62cc0190dac58f3099932d0f4c25699922198
Estevan <estebanmpg2006@gmail.com>
2025-06-06 23:07:36 -0500

    script-updt

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const rec_inf = () => {
+    let name = document.getElementById("name").value;
+    let soli = document.getElementById("sol").value;
+    let mail = document.getElementById("email").value;
+    alert("Buenos dias Señor(a) " + name + " su solicitud de " + soli + " ha sido registrada con exito, pronto recibira su respuesta al correo " + mail)
+
+}
